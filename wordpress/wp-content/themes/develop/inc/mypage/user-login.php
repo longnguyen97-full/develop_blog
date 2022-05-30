db26da04ca169757285e4fec021391714c7f457c
Liam <liam@cybridge.jp>
2022-05-30 04:33:29 +0000

    fix login

--- wordpress/wp-content/themes/develop/inc/mypage/user-login.php
+++ wordpress/wp-content/themes/develop/inc/mypage/user-login.php
@@ -1,30 +1,28 @@
 <?php
-function mp_user_login_page()
-{
-    // redirect user from wp-login to home page
-    if (basename($_SERVER['REQUEST_URI']) == "wp-login.php" && $_SERVER['REQUEST_METHOD'] == 'GET') {
-        mp_redirect_user();
-    }
-}
-add_action('init', 'mp_user_login_page');
-
 function mp_user_login_failed()
 {
-    mp_redirect_user('failed');
+    if (!empty(mp_is_user_screen())) {
+        mp_redirect_user('failed');
+        exit;
+    }
 }
 add_action('wp_login_failed', 'mp_user_login_failed');
 
-function mp_verify_username_password($user, $username, $password)
+function mp_user_login_empty($user, $username, $password)
 {
-    if ($username == "" || $password == "") {
+    if (($username == "" || $password == "") && !empty(mp_is_user_screen())) {
         mp_redirect_user('empty');
+        exit;
     }
 }
-add_filter('authenticate', 'mp_verify_username_password', 1, 3);
+add_filter('authenticate', 'mp_user_login_empty', 1, 3);
 
 function mp_user_logout()
 {
-    mp_redirect_user('false');
+    if (!empty(mp_is_user_screen())) {
+        mp_redirect_user('false');
+        exit;
+    }
 }
 add_action('wp_logout', 'mp_user_logout');
 
@@ -33,4 +31,17 @@ function mp_redirect_user($status = '')
     $with_status = !empty($status) ? "?login={$status}" : '';
     wp_redirect(home_url('/login/') . $with_status);
     exit;
+}
+
+function mp_is_user_screen()
+{
+    $referrer = (isset($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : $_SERVER['PHP_SELF'];
+    $referrer = add_query_arg('result', 'failed', $referrer);
+    $referrer = add_query_arg('username', $username, $referrer);
+
+    if (!empty($referrer) && !strstr($referrer, 'wp-login') && !strstr($referrer, 'wp-admin')) {
+        return true;
+    }
+
+    return false;
 }
