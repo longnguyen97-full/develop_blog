e82fb308858a713316853dcba01e627b002bac37
liam <liam@cybridge.jp>
2023-04-21 07:53:23 +0700

    [Fix] meta title

--- wordpress/wp-content/themes/develop/header.php
+++ wordpress/wp-content/themes/develop/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Avision</title>
+    <title><?php echo get_bloginfo(); ?></title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="Demo project">
